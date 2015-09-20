439c2098628579a4c84ba6fa6e9261fd4d6de339
fepel <felix.pelletier.01@gmail.com>
2015-09-20 15:46:23 -0400

    Help pages were added. We still need the layout for the fights page. @FredFaust, you might look at my changes. .nsproj is used in VS, so you can ignore it.

--- constants/url.js
+++ constants/url.js
@@ -1,6 +1,7 @@
 module.exports = {
   HOME: '/home',
   HELP: '/help',
+  HELP_TOPIC: '/help/:topic',
   START: '/start',
   PAGES: '/pages/:pagenumber',
   PAGES_TODO: '/pages/todo'
--- routes/pages.js
+++ routes/pages.js
@@ -3,23 +3,39 @@ var express = require('express'),
     url = require('../constants/url');
 
 router.get(url.HOME, function(req, res) {
-  res.render('pages/home', { title: 'THE GAMEBOOK OF MASTER KAI' });
+    res.render('pages/home', { title: 'Le Livre de Jeu du Maitre Kai' });
 });
 
-router.get(url.HELP, function(req, res) {
-  res.render('pages/help', { title: 'HELP' });
+router.get(url.HELP, function (req, res) {
+    res.render('pages/help', { title: 'Aide' });
+});
+
+router.get(url.HELP_TOPIC, function (req, res) {
+    var knownTopics = {};
+    knownTopics['disciplines'] = 'Disciplines Kai';
+    knownTopics['equipment'] = 'Equipement';
+    knownTopics['fights'] = 'Combats';
+    
+    var helpTopic = req.params.topic;
+
+    if (knownTopics[helpTopic] === undefined) {
+        res.render('pages/help/unknownTopic', { title: 'Aide', topic: helpTopic });
+        return;
+    }
+
+    res.render('pages/help/' + req.params.topic, { title: 'Aide', topic: knownTopics[helpTopic] }); 
 });
 
 router.get(url.START, function(req, res) {
-  res.render('pages/start', { title: 'START' });
+    res.render('pages/start', { title: 'Depart' });
 });
 
 router.get(url.PAGES, function(req, res) {
-  res.render('pages/book/page' + req.params.pagenumber, { title: 'PAGES', pageNumber: req.params.pagenumber });
+    res.render('pages/book/page' + req.params.pagenumber, { title: 'Pages', pageNumber: req.params.pagenumber });
 });
 
 router.get(url.PAGES_TODO, function(req, res) {
-  res.render('pages/book/pageTODO', { title: 'PAGES' });
+    res.render('pages/book/pageTODO', { title: 'Pages' });
 });
 
 module.exports = router;

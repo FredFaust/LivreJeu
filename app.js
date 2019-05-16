de99a655c798a85de3e177f9cbdba772242a5b96
Félix Pelletier <felix.pelletier.01@gmail.com>
2019-05-15 22:45:17 -0400

    Prefixed unused parameters with underscore

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
-    redirect = require('express-redirect'),
-    session = require('express-session'),
-    path = require('path'),
-    favicon = require('serve-favicon'),
-    logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    router = require('./routes/router'),
-    _ = require('underscore');
+redirect = require('express-redirect'),
+  session = require('express-session'),
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  router = require('./routes/router'),
+  _ = require('underscore');
 
 var app = express();
 redirect(app);
@@ -38,7 +38,7 @@ app.use('/img', express.static(path.join(__dirname, 'public/resources/images')))
 app.use(router);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (_req, _res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -49,23 +49,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('pages/error', {
-        message: err.message,
-        error: err
+  app.use(function (err, _req, res, _next) {
+    res.status(err.status || 500);
+    res.render('pages/error', {
+      message: err.message,
+      error: err
     });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('pages/error', {
-        message: err.message,
-        error: {}
-    });
+app.use(function (err, _req, res, _next) {
+  res.status(err.status || 500);
+  res.render('pages/error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
